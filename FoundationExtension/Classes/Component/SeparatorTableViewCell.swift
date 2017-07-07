6b0c42172c701f573a9d2aacc69981d50f15ebc8
jerry <445242970@qq.com>
2017-07-07 18:57:50 +0800

    make SeparatorTableViewCell open

--- FoundationExtension/Classes/Component/SeparatorTableViewCell.swift
+++ FoundationExtension/Classes/Component/SeparatorTableViewCell.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-public class SeparatorTableViewCell: UITableViewCell {
+open class SeparatorTableViewCell: UITableViewCell {
     
     public enum SeparatorPosition {
         case top
